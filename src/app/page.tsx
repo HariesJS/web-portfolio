3db5556fba80f114eb8354b731b9241bd66aa596
lofmty <lofmty@gmail.com>
2025-06-19 06:23:33 +0300

    upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import React, {useEffect, useLayoutEffect, useRef, useState} from "react"
 import gsap from "gsap"
 import {ScrollTrigger} from "gsap/ScrollTrigger"
 import {ScrollToPlugin} from "gsap/ScrollToPlugin"
-import {BottomIcon} from "@/components/Header/icons"
+import {
+    BottomIcon,
+    GithubIcon,
+    LinkedInIcon,
+    MailIcon,
+    TelegramIcon,
+} from "@/components/Header/icons"
 import {Image} from "@heroui/image"
 import {Accordion, AccordionItem} from "@heroui/accordion"
 import {Divider} from "@heroui/divider"
@@ -13,6 +19,8 @@ import {motion} from "framer-motion"
 import {Header} from "@/components/Header"
 import {skillsData} from "./const"
 import {Theme} from "@/styles/theme"
+import {Link} from "@heroui/link"
+import {siteConfig} from "@/config/site"
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
@@ -288,7 +296,7 @@ export default function Home() {
                     opacity: 0.5,
                 }}
             ></section>
-            <section ref={toolsRef} className="section">
+            <section ref={toolsRef} className="tools">
                 <p
                     style={{
                         textAlign: "center",
@@ -554,20 +562,161 @@ export default function Home() {
                             <Code color="success">
                                 subscriptions-transport-ws
                             </Code>
-                            <Code color="success"></Code>
-                            <Code color="success"></Code>
-                            <Code color="success"></Code>
                         </div>
                     </AccordionItem>
                 </Accordion>
             </section>
-            <section
-                ref={contactsRef}
-                className="section"
-                style={{
-                    background: "green",
-                }}
-            ></section>
+            <section ref={contactsRef} className="contacts" style={{}}>
+                <p
+                    style={{
+                        textAlign: "center",
+                        fontSize: 30,
+                        marginBottom: 10,
+                    }}
+                >
+                    Contacts
+                </p>
+                <div
+                    style={{
+                        textAlign: "center",
+                        backdropFilter: "blur(10px)",
+                        backgroundColor: "rgba(255, 255, 255, 0.1)", // полупрозрачный фон
+                        padding: "20px",
+                        borderRadius: "12px",
+                        marginBottom: 40,
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <span style={{fontWeight: "bold"}}>GitHub:</span>{" "}
+                        <Link
+                            href={"https://github.com/HariesJS"}
+                            target="_blank"
+                            style={{
+                                textDecoration: "underline",
+                                color: Theme.mainColors.yellow,
+                            }}
+                        >
+                            https://github.com/HariesJS
+                        </Link>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <span style={{fontWeight: "bold"}}>LinkedIn:</span>{" "}
+                        <Link
+                            href={
+                                "https://www.linkedin.com/in/evgeniy-chepurnoy/"
+                            }
+                            target="_blank"
+                            style={{
+                                textDecoration: "underline",
+                                color: Theme.mainColors.yellow,
+                            }}
+                        >
+                            https://www.linkedin.com/in/evgeniy-chepurnoy
+                        </Link>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <span style={{fontWeight: "bold"}}>Telegram:</span>{" "}
+                        <Link
+                            href={"https://t.me/evg_9"}
+                            target="_blank"
+                            style={{
+                                textDecoration: "underline",
+                                color: Theme.mainColors.yellow,
+                            }}
+                        >
+                            https://t.me/evg_9
+                        </Link>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <span style={{fontWeight: "bold"}}>Email:</span>{" "}
+                        <Link
+                            href={"mailto:lofmty@gmail.com"}
+                            target="_blank"
+                            style={{
+                                textDecoration: "underline",
+                                color: Theme.mainColors.yellow,
+                            }}
+                        >
+                            lofmty@gmail.com
+                        </Link>
+                    </div>
+                </div>
+                <div
+                    className="backdrop-blur-sm bg-black/50"
+                    style={{
+                        width: "100%",
+                        padding: "20px",
+                        borderRadius: "20px",
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            gap: 10,
+                        }}
+                    >
+                        <Link
+                            isExternal
+                            aria-label="Github"
+                            href={siteConfig.links.github}
+                        >
+                            <GithubIcon className="text-default-500" />
+                        </Link>
+                        <Link
+                            isExternal
+                            aria-label="LinkedIn"
+                            href={siteConfig.links.linkedIn}
+                        >
+                            <LinkedInIcon />
+                        </Link>
+                        <Link
+                            isExternal
+                            aria-label="Telegram"
+                            href={siteConfig.links.telegram}
+                        >
+                            <TelegramIcon />
+                        </Link>
+                        <Link
+                            isExternal
+                            aria-label="Mail"
+                            href={siteConfig.links.mail}
+                        >
+                            <MailIcon />
+                        </Link>
+                    </div>
+                </div>
+            </section>
             <style jsx global>{`
                 .section {
                     position: relative;
@@ -577,6 +726,26 @@ export default function Home() {
                     display: flex;
                     flex-direction: column;
                     // justify-content: center;
+                    align-items: center;
+                    overflow: hidden;
+                }
+
+                .tools {
+                    position: relative;
+                    width: 100%;
+                    padding: 20px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    overflow: hidden;
+                }
+
+                .contacts {
+                    position: relative;
+                    width: 100%;
+                    padding: 20px;
+                    display: flex;
+                    flex-direction: column;
                     align-items: center;
                     overflow: hidden;
                 }
