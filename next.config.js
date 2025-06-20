d83aa23c5bdc7a1d8e84815bb809177a45323a37
lofmty <lofmty@gmail.com>
2025-06-20 06:54:48 +0300

    fff

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@ const createNextIntlPlugin = require("next-intl/plugin")
 
 const withNextIntl = createNextIntlPlugin()
 
+const isProd = process.env.NODE_ENV === "production"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: isProd ? "/web-portfolio" : "",
+    assetPrefix: isProd ? "/web-portfolio/" : "",
+}
 
 module.exports = withNextIntl(nextConfig)
